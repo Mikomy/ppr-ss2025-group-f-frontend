b5c72a6ccf31d468ca3231b54543b6ef85780123
Miklos Komlosy <2220859001@fh-burgenland.at>
2025-03-27 21:52:23 +0100

    feat: add history and statistic pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,13 @@
 import { Routes } from '@angular/router';
 import {HomePageComponent} from './pages/home-page/home-page.component';
+import {
+  HistoricalAnalysisComponentComponent
+} from './pages/historical-analysis-component/historical-analysis-component.component';
+import {StatisticsComponent} from './pages/statistics/statistics.component';
 
 export const routes: Routes = [
   { path: '', component: HomePageComponent },
+  { path: 'history', component: HistoricalAnalysisComponentComponent },
+  { path: 'statistics', component: StatisticsComponent }
+
 ];
---
+++ src/app/pages/historical-analysis-component/historical-analysis-component.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-historical-analysis-component',
+  standalone: true,
+  imports: [],
+  templateUrl: './historical-analysis-component.component.html',
+  styleUrl: './historical-analysis-component.component.scss'
+})
+export class HistoricalAnalysisComponentComponent {
+
+}
