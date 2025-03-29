0e5d26c4fa6274c5e6802e258eb61856d2b9c8d5
souri <souri@wp-entwickler.at>
2025-03-29 13:09:16 +0100

    bug: bugfix, vergessen ondestroy rauszunehmen

--- src/app/pages/statistics/statistics.component.ts
+++ src/app/pages/statistics/statistics.component.ts
@@ -11,7 +11,7 @@ import { Subject } from 'rxjs';
   templateUrl: './statistics.component.html',
   styleUrl: './statistics.component.scss'
 })
-export class StatisticsComponent implements OnInit, OnDestroy {
+export class StatisticsComponent implements OnInit {
 
   measurements: InfluxMeasurement[] = [];
   
