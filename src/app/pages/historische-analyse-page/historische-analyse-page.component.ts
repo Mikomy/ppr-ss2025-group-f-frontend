56de29f13633f12026e8d860fcda5a829181c7f3
Miklos Komlosy <2220859001@fh-burgenland.at>
2025-04-09 23:24:30 +0200

    fix: add standalone

--- src/app/pages/historische-analyse-page/historische-analyse-page.component.ts
+++ src/app/pages/historische-analyse-page/historische-analyse-page.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-historische-analyse-page',
+  standalone: true,
   imports: [],
   templateUrl: './historische-analyse-page.component.html',
   styleUrl: './historische-analyse-page.component.scss',
--- src/app/pages/tabellenansicht-page/tabellenansicht-page.component.ts
+++ src/app/pages/tabellenansicht-page/tabellenansicht-page.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-tabellenansicht-page',
+  standalone: true,
   imports: [],
   templateUrl: './tabellenansicht-page.component.html',
   styleUrl: './tabellenansicht-page.component.scss',
